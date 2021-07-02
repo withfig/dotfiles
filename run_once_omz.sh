9ad25330a3487c3cf32f4376bbefe9e1464868f5
Matt Schrage <matthewschrage@gmail.com>
2021-07-01 18:08:58 -0700

    move omz plugin installation script

--- run_once_omz.sh
+++ run_once_omz.sh
@@ -3,3 +3,8 @@
 KEEP_ZSHRC=yes
 CHSH=no
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+# install plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
